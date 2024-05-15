1d3ce46f24cd1b5435e951abf28e213b91906a74
sharifian-m <mzh.sharifian@gmail.com>
2024-05-16 00:32:14 +0330

    test communicate with front

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
 
-  getHello(): { response: any } {
+  getHello(): { response: any  } {
     return {response:'Hello World!!!!!!!!'};
   }
 }
